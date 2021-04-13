cbdb7205a6741b9d101522b2d4d1e0c66fb473a6
adavila <adavila@optimeconsulting.com>
2021-04-12 23:59:02 -0500

    Added styles and backend communication in Us intern, added new icons.

--- src/templates/us.js
+++ src/templates/us.js
@@ -1,6 +1,5 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
-import Img from "gatsby-image"
+import { graphql } from "gatsby"
 import parse from "html-react-parser"
 
 import Layout from "../components/layout"
@@ -10,6 +9,11 @@ import TitleIntern from "../components/titleIntern"
 const Us = ({data}) => {
 
   const detail = data.wpPage
+  const mision = data.wpPage.ACFUs.additionalfields[0]
+  const vision = data.wpPage.ACFUs.additionalfields[1]
+  const principles = data.wpPage.ACFUs.additionalfields[2]
+  const values = data.wpPage.ACFUs.additionalfields[3]
+  const videos = data.wpPage.ACFUs.videos
 
   return (
     <Layout>
@@ -22,6 +26,82 @@ const Us = ({data}) => {
           </div>
         </div>
       </div>
+      <section className="bg-blue-middle">
+        <div className="wrap_content container pt-3 pb-5">
+          <div className="row">
+              <div className="col-12 txt-white">
+                <h2 className="txt-white mision-title pt-5">{mision.title}</h2>
+                {parse(mision.text)}
+              </div>
+              <div className="col-12 txt-white">
+                <h2 className="txt-white vision-title pt-5">{vision.title}</h2>
+                {parse(vision.text)}
+              </div>
+          </div>
+        </div>
+      </section>
+      {videos && 
+        <section className="bg-gray-clear">
+          <div className="wrap_content container py-5">
+            <div className="row">
+              <div className="col-12 pb-3">
+                <h2>Videos</h2>
+              </div>
+              {videos.map(({link, title}, i) => {
+                let embedId = link.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/)[1];
+
+                return (
+                  <div className="col-md-6" key={i}>
+                    <iframe
+                      width="100%"
+                      height="480"
+                      src={`https://www.youtube.com/embed/${embedId}`}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      title={title}
+                    />
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+      }
+      <section className="py-5">
+        <div className="wrap_content container py-5">
+          <div className="row">
+            <div className="col-12">
+              <div className="accordion" id="accordionExample">
+                <div class="accordion-item pb-5">
+                  <h2 class="accordion-header txt-white" id="headingOne">
+                    <button class="accordion-button bg-blue-middle py-4 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                      {principles.title}
+                    </button>
+                  </h2>
+                  <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                      {parse(principles.text)}
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header txt-white" id="headingTwo">
+                    <button class="accordion-button bg-blue-middle py-4 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                      {values.title}
+                    </button>
+                  </h2>
+                  <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                      {parse(values.text)}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
@@ -36,6 +116,16 @@ export const pageQuery = graphql`
       uri
       title
       content
+      ACFUs {
+        additionalfields {
+          text
+          title
+        }
+        videos {
+          link
+          title
+        }
+      }
       featuredImage {
         node {
           localFile {
