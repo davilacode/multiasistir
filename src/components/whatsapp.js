b94de8228cc6b54b0e51d8d2bda4df6c7e1ac930
davdevelopment <andersonfidel.14@gmail.com>
2022-03-29 10:51:18 -0500

    Added new pse pay button

--- src/components/whatsapp.js
+++ src/components/whatsapp.js
@@ -1,17 +1,24 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import imgWhatsapp from "../../content/assets/icon-wp.png"
+import imgPse from "../../content/assets/icon-pse.png"
 
 const Whatsapp = () => {
 
     const data = useStaticQuery(query)
 
     const whatsappNumber = data.wp.siteGeneralSettings.ACFWhatsapp.whatsapp
+    const pseLink = data.wp.siteGeneralSettings.ACFPse.link
 
     return (
-        <a id="whatsapp" href={`https://api.whatsapp.com/send/?phone=57${whatsappNumber}`} title="WhatsApp" target="_blank" rel="noreferrer noopener">
-            <img src={imgWhatsapp} alt="WhatsApp"/>
-        </a>
+        <>
+            <a id="whatsapp" href={`https://api.whatsapp.com/send/?phone=57${whatsappNumber}`} title="WhatsApp" target="_blank" rel="noreferrer noopener">
+                <img src={imgWhatsapp} alt="WhatsApp"/>
+            </a>
+            <a id="pse" href={`${pseLink}`} title="PSE" target="_blank" rel="noreferrer noopener">
+                <img src={imgPse} alt="PSE"/>
+            </a>
+        </>
     )
 }
 
@@ -23,6 +30,9 @@ const query = graphql`
             siteGeneralSettings {
                 ACFWhatsapp {
                     whatsapp
+                }
+                ACFPse {
+                    link
                 }
             }
         }
